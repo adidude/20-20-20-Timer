83115b596a022447e980d278f3e9e3eb6244342a
Aditya Kumar Menon <menon.or.adithya@gmail.com>
2022-02-15 23:51:21 +0000

    Added timer countdown.

--- src/Timer.js
+++ src/Timer.js
@@ -1,46 +1,64 @@
 var time = document.getElementById("Timer");
-var min = 2;
-var seconds = 0;
+var minutes = 20;
+var second = 0;
 
-// TODO: Make if statements more performant/less nested
-var timer = setInterval(function(){
-	if(seconds == 0 && min == 0)
-	{
-		clearInterval(timer);
-		time.innerHTML = "00:00"
-	}
-	else
-	{
-		var strTime = "";
-		if(min < 10)
+startTimer(minutes,second)
+function startTimer(min,seconds)
+{
+	// TODO: Make if statements more performant/less nested
+	var timer = setInterval(function(){
+		if(seconds == 0 && min == 0)
 		{
-			strTime = '0' + min + ':'; 
+			clearInterval(timer);
+			showBreakButton()
+			time.innerHTML = "00:00"
 		}
 		else
 		{
-			strTime = min + ':';
-		}
+			var strTime = "";
+			if(min < 10)
+			{
+				strTime = '0' + min + ':'; 
+			}
+			else
+			{
+				strTime = min + ':';
+			}
 
-		if(seconds < 10)
+			if(seconds < 10)
+			{
+				strTime = strTime + '0' + seconds;
+			}
+			else
+			{
+				strTime = strTime + seconds;
+			}
+
+			time.innerHTML = strTime;
+		}
+		
+		if(seconds == 0)
 		{
-			strTime = strTime + '0' + seconds;
+			min = min - 1;
+			seconds = 59;
 		}
 		else
 		{
-			strTime = strTime + seconds;
+			seconds = seconds - 1;
 		}
+		
+	},1000);
+}
 
-		time.innerHTML = strTime;
-	}
-	
-	if(seconds == 0)
-	{
-		min = min - 1;
-		seconds = 59
-	}
-	else
-	{
-		seconds = seconds - 1;
-	}
-	
-},1000);
+function showBreakButton()
+{
+	var breaker = document.getElementById("breakBut");
+	breaker.style.visibility = "visible"
+}
+
+function hideBreakButton()
+{
+	var breaker = document.getElementById("breakBut");
+	breaker.style.visibility = "hidden";
+	startTimer(0,20);
+}
